7cd8f62a49557e71f7f6d7366ab703d88d465544
ChristinaShatskikh <morenita1907@gmail.com>
2022-05-04 22:20:34 +0300

    Update 2022-05-04T19:20:28.304Z

--- source/js/instructors-block.js
+++
@@ -1,7 +0,0 @@
-const openTextButton = document.querySelector(".instructors__further-button");
-const text = document.querySelector(".instructors__text-last");
-
-openTextButton.addEventListener("click", () => {
-  openTextButton.classList.toggle("instructors__further-button--off")
-  text.classList.toggle("instructors__text-last--hidden");
-})
